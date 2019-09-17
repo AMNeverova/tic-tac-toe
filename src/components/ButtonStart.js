12b163900f6990debed317c80a0b99d37c472e64
Anastasiya Neverova <anastacia1302@yandex.ru>
2019-09-17 18:53:02 +0300

    mobile field amended

---
+++ src/components/ButtonStart.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {FormattedMessage} from 'react-intl';
+
+class ButtonStart extends React.Component {
+    constructor (){
+        super();
+        this.handleClick = this.handleClick.bind(this);
+      }
+      
+    handleClick () {
+        this.props.buttonStartAC()
+    }
+
+    render () {
+        return (
+            <div className='start'>
+                {this.props.state? 
+                    <button className='button-start' onClick={this.handleClick}><FormattedMessage id='startAgainButton' /></button> : 
+                    <div><button className='button-start' onClick={this.handleClick}><FormattedMessage id='button-start' /></button>
+                    <div className='warning'><FormattedMessage id='warning' /></div></div> }
+            </div>
+        )
+    }
+}
+export default ButtonStart; 
